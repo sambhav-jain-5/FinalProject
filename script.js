ce360f10304b6fef4abb9887eab14ead78d9a5cd
sambhav-jain-5 <sambhavjain2499@gmail.com>
2025-05-07 23:19:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007bff',
+        accent: '#ffcc00',
+        schoolRed: '#d32f2f',
+        schoolBlue: '#1976d2',
+        schoolGreen: '#388e3c',
+      },
+    },
+  },
+};
