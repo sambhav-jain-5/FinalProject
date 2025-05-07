1cbb37800027cc6d752ab1349c4ed042f3931afe
sambhav-jain-5 <sambhavjain2499@gmail.com>
2025-05-08 01:07:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      colors: {
-        primary: '#007bff',
-        accent: '#ffcc00',
-        schoolRed: '#d32f2f',
-        schoolBlue: '#1976d2',
-        schoolGreen: '#388e3c',
-      },
-    },
-  },
-};
+
